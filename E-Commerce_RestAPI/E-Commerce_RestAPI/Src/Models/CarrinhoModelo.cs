bb2fd17cf7e8916175c2e598323bd7d06826b241
rodrigox98 <rodrigo.silva31@hotmail.com>
2022-10-06 21:28:26 -0300

    Iniciando projeto, criando modelos do banco de dados

---
+++ E-Commerce_RestAPI/E-Commerce_RestAPI/Src/Models/CarrinhoModelo.cs
@@ -0,0 +1,6 @@
+﻿namespace E_Commerce_RestAPI.Src.Models
+{
+    public class CarrinhoModelo
+    {
+    }
+}
---
+++ E-Commerce_RestAPI/E-Commerce_RestAPI/Src/Models/UsuarioModelo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace E_Commerce_RestAPI.Src.Models
+{
+    [Table("tb_usuarios")] //Define que a classe usuário será representada no banco de dados como  'tb_usuarios'
+    public class UsuarioModelo
+    {
+        #region atributos
+
+        [Key] //Define a primary key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //Define que o atributo ID será auto-incrementável
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+
+
+        [JsonIgnore, InverseProperty("Comprador")]
+        public List<CarrinhoModelo> Compras { get; set; }
+        #endregion
+    }
+}
